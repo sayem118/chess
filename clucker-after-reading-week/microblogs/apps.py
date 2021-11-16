de99d5ec5ca4b1133d1e633ca689b1aafe4346bb
sayem118 <91573543+sayem118@users.noreply.github.com>
2021-11-16 13:57:10 +0000

    Delete clucker-after-reading-week directory

--- clucker-after-reading-week/microblogs/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MicroblogsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'microblogs'
--- clucker-after-reading-week/microblogs/tests/helpers.py
+++
@@ -1,3 +0,0 @@
-class LogInTester:
-    def _is_logged_in(self):
-        return '_auth_user_id' in self.client.session.keys()
