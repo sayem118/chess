0ef354d6e4d5e3341f71284e690cb7945cf42843
Wilson Mak <cheung.mak@kcl.ac.uk>
2021-11-20 21:53:30 +0000

    Change User model so that email is used as username
    - Create a custom Manager for the altered User model
    - Write test of the User model

--- clubs/models.py
+++ clubs/models.py
@@ -1,6 +1,42 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.core.validators import RegexValidator
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+class UserManager(BaseUserManager):
+    """Model manager for User model with no username field."""
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
 
 class User(AbstractUser):
     """User model used for authentication and clubs authoring."""
@@ -12,13 +48,18 @@ class User(AbstractUser):
     ROLE_CHOICES = (
         (APPLICANT, 'Applicant'),
         (MEMBER, 'Member'),
-        (OFFICER,'Officer'),
-        (OWNER,'Owner'),
+        (OFFICER, 'Officer'),
+        (OWNER, 'Owner'),
     )
 
-    username = models.EmailField(unique=True, blank=False)
+    username = None
+    email = models.EmailField(unique=True, blank=False)
     first_name = models.CharField(max_length=50, blank=False)
     last_name = models.CharField(max_length=50, blank=False)
     bio = models.CharField(max_length=520, blank=True)
-    role = models.PositiveSmallIntegerField(choices = ROLE_CHOICES, default = APPLICANT)
-#jacky12 = User.objects.create_user(username = 'jack234y23@gmail.com', first_name = 'john', last_name = 'doe', password = 'Password123', bio = 'hi i hope this works', role = 2)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, default=APPLICANT)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
---
+++ clubs/tests/models/test_user_model.py
@@ -0,0 +1,133 @@
+"""Unit tests for the User model."""
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+from clubs.models import User
+
+
+# Create your tests here.
+class UserModelTestCase(TestCase):
+    """Unit tests for the User model."""
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email="johndoe@example.org",
+            first_name="John",
+            last_name="Doe",
+            bio="The quick brown fox jumped over the lazy dog."
+        )
+
+    def test_valid_user(self):
+        self._assert_user_is_valid()
+
+    def test_first_name_must_not_be_blank(self):
+        self.user.first_name = ''
+        self._assert_user_is_invalid()
+
+    def test_first_name_need_not_be_unique(self):
+        second_user = User.objects.create_user(
+            email="janedoe@example.org",
+            first_name="Jane",
+            last_name="Doe",
+            bio="This is my bio."
+        )
+        self.user.first_name = second_user.first_name
+        self._assert_user_is_valid()
+
+    def test_first_name_may_contain_50_characters(self):
+        self.user.first_name = 'x' * 50
+        self._assert_user_is_valid()
+
+    def test_first_name_must_not_contain_more_than_50_characters(self):
+        self.user.first_name = 'x' * 51
+        self._assert_user_is_invalid()
+
+    def test_last_name_must_not_be_blank(self):
+        self.user.last_name = ''
+        self._assert_user_is_invalid()
+
+    def test_last_name_need_not_be_unique(self):
+        second_user = User.objects.create_user(
+            email="janedoe@example.org",
+            first_name="Jane",
+            last_name="Doe",
+            bio="This is my bio."
+        )
+        self.user.last_name = second_user.last_name
+        self._assert_user_is_valid()
+
+    def test_last_name_may_contain_50_characters(self):
+        self.user.last_name = 'x' * 50
+        self._assert_user_is_valid()
+
+    def test_last_name_must_not_contain_more_than_50_characters(self):
+        self.user.last_name = 'x' * 51
+        self._assert_user_is_invalid()
+
+    def test_email_must_not_be_blank(self):
+        self.user.email = ''
+        self._assert_user_is_invalid()
+
+    def test_email_must_be_unique(self):
+        second_user = User.objects.create_user(
+            email="janedoe@example.org",
+            first_name="Jane",
+            last_name="Doe",
+            bio="This is my bio."
+        )
+        self.user.email = second_user.email
+        self._assert_user_is_invalid()
+
+    def test_email_must_contain_username(self):
+        self.user.email = '@example.org'
+        self._assert_user_is_invalid()
+
+    def test_email_must_contain_at_symbol(self):
+        self.user.email = 'johndoe.example.org'
+        self._assert_user_is_invalid()
+
+    def test_email_must_contain_domain_name(self):
+        self.user.email = 'johndoe@.org'
+        self._assert_user_is_invalid()
+
+    def test_email_must_contain_domain(self):
+        self.user.email = 'johndoe@example'
+        self._assert_user_is_invalid()
+
+    def test_email_must_not_contain_more_than_one_at(self):
+        self.user.email = 'johndoe@@example.org'
+        self._assert_user_is_invalid()
+
+    def test_bio_may_be_blank(self):
+        self.user.bio = ''
+        self._assert_user_is_valid()
+
+    def test_bio_need_not_be_unique(self):
+        second_user = User.objects.create_user(
+            email="janedoe@example.org",
+            first_name="Jane",
+            last_name="Doe",
+            bio="This is my bio."
+        )
+        self.user.bio = second_user.bio
+        self._assert_user_is_valid()
+
+    def test_bio_may_contain_520_characters(self):
+        self.user.bio = 'x' * 520
+        self._assert_user_is_valid()
+
+    def test_bio_must_not_contain_more_than_520_characters(self):
+        self.user.bio = 'x' * 521
+        self._assert_user_is_invalid()
+
+    def test_default_role_is_applicant(self):
+        self.assertEqual(self.user.role, User.APPLICANT)
+
+    def _assert_user_is_valid(self):
+        try:
+            self.user.full_clean()
+        except ValidationError:
+            self.fail('Test user should be valid')
+
+    def _assert_user_is_invalid(self):
+        with self.assertRaises(ValidationError):
+            self.user.full_clean()
