4a31fe345772795335f9fca17ca02d7e6e898c39
Sayem Ahmed <sayem.1.ahmed@kcl.ac.uk>
2021-11-20 14:17:32 +0000

    added users without tests

--- clubs/models.py
+++ clubs/models.py
@@ -1,3 +1,31 @@
+from django.core.validators import RegexValidator
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    """User model used for authentication and clubs authoring."""
+    APPLICANT = 0
+    MEMBER = 1
+    OFFICER = 2
+    OWNER = 3
+
+    ROLE_CHOICES = (
+        (APPLICANT, 'Applicant'),
+        (MEMBER, 'Member'),
+        (OFFICER,'Officer'),
+        (OWNER,'Owner'),
+    )
+
+    username = models.CharField(
+        max_length=30,
+        unique=True,
+        validators=[RegexValidator(
+            regex=r'^@\w{3,}$',
+            message='Username must consist of @ followed by at least three alphanumericals'
+        )]
+    )
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
+    email = models.EmailField(unique=True, blank=False)
+    bio = models.CharField(max_length=520, blank=True)
+    role = models.PositiveSmallIntegerField(choices = ROLE_CHOICES, default = APPLICANT)
