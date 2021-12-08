d7205eb56015f62d59893d682f0ad702b5f7019c
Aaron Gomes <aaron.gomes@kcl.ac.uk>
2021-12-08 21:50:59 +0000

    Add tests for membership constraints

--- clubs/tests/models/test_membership_model.py
+++ clubs/tests/models/test_membership_model.py
@@ -11,12 +11,16 @@ class MembershipModelTestCase(TestCase):
 
     fixtures = [
         'clubs/tests/fixtures/users/default_user.json',
+        'clubs/tests/fixtures/users/other_users.json',
         'clubs/tests/fixtures/clubs/default_club.json',
+        'clubs/tests/fixtures/clubs/other_clubs.json'
     ]
 
     def setUp(self):
         self.user = User.objects.get(email='johndoe@example.org')
+        self.other_user = User.objects.get(email='janedoe@example.org')
         self.club = Club.objects.get(name="Chess Club")
+        self.other_club = Club.objects.get(name="The Royal Rooks")
 
     def test_user_cannot_be_blank(self):
         membership = Membership(user=None, club=self.club, role=Membership.OWNER)
@@ -45,3 +49,17 @@ class MembershipModelTestCase(TestCase):
             Membership.objects.get(user=self.user, club=self.club, role=Membership.OWNER)
         except ObjectDoesNotExist:
             self.fail('Test membership should be valid')
+
+    """def test_user_cannot_have_more_than_one_role_in_a_club(self):
+        membership = Membership(user=self.user, club=self.club, role=Membership.OWNER)
+        membership.save()
+        fail_membership = Membership(user=self.user, club=self.other_club, role=Membership.MEMBER)
+        with self.assertRaises(IntegrityError):
+            fail_membership.save()
+
+    def test_cannot_have_more_than_one_owner_for_a_club(self):
+        membership = Membership(user=self.user, club=self.club, role=Membership.OWNER)
+        membership.save()
+        fail_membership = Membership(user=self.other_user, club=self.club, role=Membership.OWNER)
+        with self.assertRaises(IntegrityError):
+            fail_membership.save()"""
