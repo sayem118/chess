a4a8e177e3ba140cc97148dcb210dbf2024b729a
Aaron Gomes <aaron.gomes@kcl.ac.uk>
2021-12-08 17:35:33 +0000

    Add membership model tests

---
+++ clubs/tests/models/test_membership_model.py
@@ -0,0 +1,43 @@
+"""Unit tests for the Membership model."""
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
+from django.test import TestCase
+
+from clubs.models import User, Club, Membership
+
+
+class MembershipModelTestCase(TestCase):
+    """Unit tests for the Membership model."""
+
+    fixtures = [
+        'clubs/tests/fixtures/users/default_user.json',
+        'clubs/tests/fixtures/clubs/default_club.json',
+    ]
+
+    def setUp(self):
+        self.user = User.objects.get(email='johndoe@example.org')
+        self.club = Club.objects.get(name="Chess Club")
+
+    def test_user_cannot_be_blank(self):
+        membership = Membership(user=None, club=self.club, role=Membership.OWNER)
+        with self.assertRaises(IntegrityError):
+            membership.save()
+
+    def test_club_cannot_be_blank(self):
+        membership = Membership(user=self.user, club=None, role=Membership.OWNER)
+        with self.assertRaises(IntegrityError):
+            membership.save()
+
+    def test_role_cannot_be_blank(self):
+        membership = Membership(user=self.user, club=self.club, role=None)
+        with self.assertRaises(IntegrityError):
+            membership.save()
+
+    def test_siccessful_membership_creation(self):
+        membership = Membership(user=self.user, club=self.club, role=Membership.OWNER)
+        try:
+            membership.save()
+        except ValidationError:
+            self.fail('Test membership should be valid')
+        except IntegrityError:
+            self.fail('Test membership should be valid')
