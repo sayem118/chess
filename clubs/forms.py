6ebd23c806c8969ed84b59be06a25cd43330ef0c
Aaron Gomes <aaron.gomes@kcl.ac.uk>
2021-12-11 10:50:10 +0000

    Refactor password view and form

--- clubs/forms.py
+++ clubs/forms.py
@@ -92,14 +92,35 @@ class PasswordForm(forms.Form):
     )
     password_confirmation = forms.CharField(label='Password confirmation', widget=forms.PasswordInput())
 
+    def __init__(self, user=None, **kwargs):
+        """Construct new form instance with a user instance."""
+        super().__init__(**kwargs)
+        self.user = user
+
     def clean(self):
         """Clean the data and generate messages for any errors."""
 
         super().clean()
+        password = self.cleaned_data.get('password')
+        if self.user is not None:
+            user = authenticate(email=self.user.email, password=password)
+        else:
+            user = None
+        if user is None:
+            self.add_error('password', "Password is invalid")
         new_password = self.cleaned_data.get('new_password')
         password_confirmation = self.cleaned_data.get('password_confirmation')
         if new_password != password_confirmation:
             self.add_error('password_confirmation', 'Confirmation does not match password.')
+
+    def save(self):
+        """Save the user's new password."""
+
+        new_password = self.cleaned_data['new_password']
+        if self.user is not None:
+            self.user.set_password(new_password)
+            self.user.save()
+        return self.user
 
 
 class CreateClubForm(forms.ModelForm):
--- clubs/views.py
+++ clubs/views.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.hashers import check_password
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import ImproperlyConfigured, ObjectDoesNotExist
 from django.http import Http404
@@ -178,16 +177,12 @@ def profile(request):
 def password(request):
     current_user = request.user
     if request.method == 'POST':
-        form = PasswordForm(data=request.POST)
+        form = PasswordForm(user=current_user, data=request.POST)
         if form.is_valid():
-            password = form.cleaned_data.get('password')
-            if check_password(password, current_user.password):
-                new_password = form.cleaned_data.get('new_password')
-                current_user.set_password(new_password)
-                current_user.save()
-                login(request, current_user)
-                messages.add_message(request, messages.SUCCESS, "Password updated!")
-                return redirect('start')
+            form.save()
+            login(request, current_user)
+            messages.add_message(request, messages.SUCCESS, "Password updated!")
+            return redirect('start')
     form = PasswordForm()
     return render(request, 'password.html', {'form': form})
 
