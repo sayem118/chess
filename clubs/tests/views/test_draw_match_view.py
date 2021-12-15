c406c85dd8bff7a28d7a4f6388d6ae599a4d4bb8
Aaron Gomes <aaron.gomes@kcl.ac.uk>
2021-12-15 19:28:57 +0000

    Add draw match view tests

--- clubs/tests/views/test_draw_match_view.py
+++ clubs/tests/views/test_draw_match_view.py
@@ -1,4 +1,4 @@
-"""Tests of the win contender one view."""
+"""Tests of the draw match view."""
 
 from django.test import TestCase
 from django.urls import reverse
@@ -8,8 +8,8 @@ from clubs.models import User, Club, Membership, Tournament, Match
 from clubs.tests.helpers import reverse_with_next
 
 
-class WinContenderOneViewTestCase(TestCase):
-    """Tests of thewin contender one view."""
+class DrawMatchViewTestCase(TestCase):
+    """Tests of the draw match view."""
 
     fixtures = [
         'clubs/tests/fixtures/users/default_user.json',
@@ -36,31 +36,49 @@ class WinContenderOneViewTestCase(TestCase):
         self.owner.select_club(self.other_club)
         self.tournament = Tournament.objects.get(name='Chess Tournament')
         self.other_tournament = Tournament.objects.get(name='Battle of the Titans')
-        self.match = Match(contender_one=self.member, contender_two=self.other_member, tournament=self.tournament, group=1, stage=1)
+        self.match = Match(contender_one=self.member, contender_two=self.other_member, tournament=self.tournament, group=1, stage=5)
         self.match.save()
-        self.url = reverse('win_contender_one', kwargs={'match_id': self.match.id})
+        self.url = reverse('draw_match', kwargs={'match_id': self.match.id})
 
-    def test_get_win_contender_one_url(self):
-        self.assertEqual(self.url, f'/win_contender_one/{self.match.id}')
+    def test_get_draw_match_url(self):
+        self.assertEqual(self.url, f'/draw_match/{self.match.id}')
 
-    def test_win_contender_one_redirects_when_not_logged_in(self):
+    def test_draw_match_redirects_when_not_logged_in(self):
         redirect_url = reverse_with_next('log_in', self.url)
         response = self.client.get(self.url)
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
-    def test_successful_get_win_contender_one(self):
+    def test_successful_draw_match_when_not_knockout(self):
         self.client.login(email=self.officer.email, password='Password123')
         response = self.client.get(self.url, follow=True)
         redirect_url = reverse('manage_tournament', kwargs={'tournament_id': self.tournament.id})
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
         self.assertTemplateUsed(response, 'manage_tournament.html')
         self.match.refresh_from_db()
-        self.assertEqual(self.match.winner, self.member)
+        self.assertEqual(self.match.winner, None)
         self.assertTrue(self.match.played)
 
-    def test_win_contender_one_with_invalid_id(self):
+    def test_successful_draw_match_when_knockout(self):
+        self.match.stage = 1
+        self.match.save()
         self.client.login(email=self.officer.email, password='Password123')
-        url = reverse('win_contender_one', kwargs={'match_id': self.match.id+9999})
+        response = self.client.get(self.url, follow=True)
+        redirect_url = reverse('manage_tournament', kwargs={'tournament_id': self.tournament.id})
+        self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
+        self.assertTemplateUsed(response, 'manage_tournament.html')
+        self.match.refresh_from_db()
+        try:
+            new_match = Match.objects.get(contender_one=self.member, contender_two=self.other_member, played=False)
+            self.assertEqual(new_match.stage, self.match.stage)
+            self.assertEqual(new_match.group, self.match.group)
+        except ObjectDoesNotExist:
+            self.fail('A new match should be scheduled')
+        self.assertEqual(self.match.winner, None)
+        self.assertTrue(self.match.played)
+
+    def test_draw_match_with_invalid_id(self):
+        self.client.login(email=self.officer.email, password='Password123')
+        url = reverse('draw_match', kwargs={'match_id': self.match.id+9999})
         response = self.client.get(url, follow=True)
         redirect_url = reverse('start')
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
